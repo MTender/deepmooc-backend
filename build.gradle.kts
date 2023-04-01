2507d7f0ac80bf44db2975b17f3e3e231f5d806b
Märt Tender <mart.tender@gmail.com>
2023-04-01 12:38:49 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("io.jooby:jooby-hibernate:$joobyVersion")
     implementation("org.postgresql:postgresql:42.5.4")
 
-    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
 
     testImplementation(kotlin("test-junit5", kotlinVersion))
     testImplementation("io.jooby:jooby-test:$joobyVersion")
